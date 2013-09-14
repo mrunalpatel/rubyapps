f083d7eacdad419cb3ed88664ecd3545beddc67f
Mrunal Patel <mrunal@outlook.com>
2013-09-14 19:56:50 -0400

    setting up ruby on rails playground - 1

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '812050174dab7d6f69b5ad764a8cfbb3ff69cb520f51b5cb47a2822d0d60e472c139ad42a605159d49b0229ec73ddb1c04fb1770309b02c3fbd4bcf36170e3f1'
